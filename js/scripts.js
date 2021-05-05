2f307dabb01a3d01ce3e8e27525fd30405ec8026
Roz <rosalindavalbrecht@gmail.com>
2021-05-04 19:06:50 -0700

    fixed it but am adding in dayEddies

--- js/scripts.js
+++ js/scripts.js
@@ -1,147 +1,140 @@
 $(function () {
 
-    let rockerboy = rockerBoyHustle(3,4);
-    console.log(rockerboy[0], rockerboy[1])
-    
-    let eddies = 0;
+    rockerboy = rockerBoyHustle(1, 4);
 
     function rockerBoyHustle(rank = 1, daysToRoll = 1) {
         let returnString;
-        let returnArray = [];
-        let roll = Math.floor(Math.random()*6);
+        var returnArray = [];
+        let roll = Math.floor(Math.random() * 6);
+        let eddies = 0;
+        // need to make an eddies per job and an eddies permanent
 
-        for (let days= 0; days = daysToRoll; days++) {
-            switch(roll) {
+        for (let days = 0; days <= daysToRoll; days++) {
+            let dayEddies;
+            switch (roll) {
                 case 1:
                     returnString = "Played a small local gig";
-                    console.log(retrunString);
-                    switch(rank){
+                    console.log(returnString);
+                    switch (rank) {
                         case 1: case 2: case 3: case 4:
                             eddies += 200;
-                            console.log(`${eddies} eb gained!`);
-                            returnArray.push(returnString, eddies)
-                            return returnArray;
+                            dayEddies = 200;
+                            console.log(`${dayEddies} eb gained!`);
+                            returnArray.push(returnString, dayEddies, eddies)
+                            break;
                         case 5: case 6: case 7:
                             eddies += 300;
-                            console.log(`${eddies} eb gained!`);
-                            returnArray.push(returnString, eddies)
-                            return returnArray;
+                            console.log(`${dayEddies} eb gained!`);
+                            returnArray.push(returnString, dayEddies, eddies)
+                            break;
                         case 8: case 9: case 10:
                             eddies += 600;
-                            console.log(`${eddies} eb gained!`);
-                            returnArray.push(returnString, eddies)
-                            return returnArray;
+                            console.log(`${dayEddies} eb gained!`);
+                            returnArray.push(returnString, dayEddies, eddies)
+                            break;
                     }
-                    break;
                 case 2:
                     returnString = `No gigs or jobs to be had this week`;
-                    console.log(retrunString)
-                    switch(rank){
+                    console.log(returnString)
+                    switch (rank) {
                         case 1: case 2: case 3: case 4:
                             eddies += 0;
-                            console.log(`${eddies} eb gained!`);
-                            returnArray.push(returnString, eddies)
-                            return returnArray;
+                            console.log(`${dayEddies} eb gained!`);
+                            returnArray.push(returnString, dayEddies, eddies)
+                            break;
                         case 5: case 6: case 7:
                             eddies += 100;
-                            console.log(`${eddies} eb gained!`);
-                            returnArray.push(returnString, eddies)
-                            return returnArray;
+                            console.log(`${dayEddies} eb gained!`);
+                            returnArray.push(returnString, dayEddies, eddies)
+                            break;
                         case 8: case 9: case 10:
                             eddies += 300;
-                            console.log(`${eddies} eb gained!`);
-                            returnArray.push(returnString, eddies)
-                            return returnArray;
+                            console.log(`${dayEddies} eb gained!`);
+                            returnArray.push(returnString, dayEddies, eddies)
+                            break;
                     }
-                    break;
                 case 3:
-                    returnString= `Played a big gig for a rich Corpo-rat or local personality`;
+                    returnString = `Played a big gig for a rich Corpo-rat or local personality`;
                     console.log(returnString);
-                    switch(rank) {
+                    switch (rank) {
                         case 1: case 2: case 3: case 4:
                             eddies += 300;
-                            console.log(`${eddies} eb gained!`);
-                            returnArray.push(returnString, eddies)
-                            return returnArray;
+                            console.log(`${dayEddies} eb gained!`);
+                            returnArray.push(returnString, dayEddies, eddies)
+                            break;
                         case 5: case 6: case 7:
                             eddies += 500;
-                            console.log(`${eddies} eb gained!`);
-                            returnArray.push(returnString, eddies)
-                            return returnArray;
+                            console.log(`${dayEddies} eb gained!`);
+                            returnArray.push(returnString, dayEddies, eddies)
+                            break;
                         case 8: case 9: case 10:
                             eddies += 800;
-                            console.log(`${eddies} eb gained!`);
-                            returnArray.push(returnString, eddies)
-                            return returnArray;
+                            console.log(`${dayEddies} eb gained!`);
+                            returnArray.push(returnString, dayEddies, eddies)
+                            break;
                     };
-                    break;
                 case 4:
                     returnString = `Got some royalties in for your most recent Data Pool download.`;
                     console.log(returnString);
-                    switch(rank) {
+                    switch (rank) {
                         case 1: case 2: case 3: case 4:
                             eddies += 300;
-                            console.log(`${eddies} eb gained!`);
-                            returnArray.push(returnString, eddies)
-                            return returnArray;
+                            console.log(`${dayEddies} eb gained!`);
+                            returnArray.push(returnString, dayEddies, eddies)
+                            break;
                         case 5: case 6: case 7:
                             eddies += 500;
-                            console.log(`${eddies} eb gained!`);
-                            returnArray.push(returnString, eddies)
-                            return returnArray;
+                            console.log(`${dayEddies} eb gained!`);
+                            returnArray.push(returnString, dayEddies, eddies)
+                            break;
                         case 8: case 9: case 10:
                             eddies += 800;
-                            console.log(`${eddies} eb gained!`);
-                            returnArray.push(returnString, eddies)
-                            return returnArray;
+                            console.log(`${dayEddies} eb gained!`);
+                            returnArray.push(returnString, dayEddies, eddies)
+                            break;
                     };
-                    break;
                 case 5:
                     returnString = `Opening act for a Big-Name group.`;
                     console.log(returnString);
-                    switch(rank) {
+                    switch (rank) {
                         case 1: case 2: case 3: case 4:
                             eddies += 300;
-                            console.log(`${eddies} eb gained!`);
-                            returnArray.push(returnString, eddies)
-                            return returnArray;
+                            console.log(`${dayEddies} eb gained!`);
+                            returnArray.push(returnString, dayEddies, eddies)
+                            break;
                         case 5: case 6: case 7:
                             eddies += 500;
-                            console.log(`${eddies} eb gained!`);
-                            returnArray.push(returnString, eddies)
-                            return returnArray;
+                            console.log(`${dayEddies} eb gained!`);
+                            returnArray.push(returnString, dayEddies, eddies)
+                            break;
                         case 8: case 9: case 10:
                             eddies += 800;
-                            console.log(`${eddies} eb gained!`);
-                            returnArray.push(returnString, eddies)
-                            return returnArray;
+                            console.log(`${dayEddies} eb gained!`);
+                            returnArray.push(returnString, dayEddies, eddies)
+                            break;
                     };
-                    break;
                 case 6:
                     returnString = `Personal appearance netted you a large fee.`;
-                    console.log(retrunString);
-                    switch(rank) {
+                    console.log(returnString);
+                    switch (rank) {
                         case 1: case 2: case 3: case 4:
                             eddies += 200;
-                            console.log(`${eddies} eb gained!`);
-                            returnArray.push(returnString, eddies)
-                            return returnArray;
+                            console.log(`${dayEddies} eb gained!`);
+                            returnArray.push(returnString, dayEddies, eddies)
+                            break;
                         case 5: case 6: case 7:
                             eddies += 300;
-                            console.log(`${eddies} eb gained!`);
-                            returnArray.push(returnString, eddies)
-                            return returnArray;
+                            console.log(`${dayEddies} eb gained!`);
+                            returnArray.push(returnString, dayEddies, eddies)
+                            break;
                         case 8: case 9: case 10:
                             eddies += 600;
-                            console.log(`${eddies} eb gained!`);
-                            returnArray.push(returnString, eddies)
-                            return returnArray;
+                            console.log(`${dayEddies} eb gained!`);
+                            returnArray.push(returnString, dayEddies, eddies)
+                            break;
                     };
-                    break;
             };
         };
-
-    
+        return returnArray;
     }
-
 });
